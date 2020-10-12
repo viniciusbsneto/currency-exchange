d26091b1b1eefbd8c751ef5f51c18200ec30ad7d
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-12 12:58:08 -0300

    feat: Add global style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <h1>Currency Converter</h1>;
-}
+  return (
+    <>
+      <h1>Currency Converter</h1>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
