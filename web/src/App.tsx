3a74448b215bed0f8590aadfe0a3855b377397ab
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-20 13:39:11 -0300

    fix: Comment currency conversion page import

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StylesProvider } from '@material-ui/core/styles';
 
-import CurrencyConversion from './pages/CurrencyConversion';
+// import CurrencyConversion from './pages/CurrencyConversion';
 import Landing from './pages/Landing';
 
 import GlobalStyle from './styles/global';
