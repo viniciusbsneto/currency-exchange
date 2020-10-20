612a05903f9988c60c5f2830d8b23fb10b962ccb
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-20 13:29:43 -0300

    feat: Improve global style

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  :root {
+    --primary-color: #84ce46;
+    --secondary-color:  #0f6c87;
+    --title-color: #322153;
+    --text-color: #101114;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -10,13 +17,19 @@ export default createGlobalStyle`
 
   body {
     background: #F0F0F5;
+    color: var(--text-color);
     color: #101114;
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, button {
-    font-family: 'Roboto', sans-serif;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
     font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    color: var(--title-color);
+    font-family: Roboto;
   }
 
   button {
