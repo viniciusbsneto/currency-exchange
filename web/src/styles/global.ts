011d8d44687a153c93043a425288888013054e76
Vinícius Neto <viniciusbsneto@gmail.com>
2020-10-21 14:23:43 -0300

    feat: Add responsive style props to global style

--- web/src/styles/global.ts
+++ web/src/styles/global.ts
@@ -2,10 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   :root {
+    font-size: 60%;
+
     --primary-color: #84ce46;
     --secondary-color:  #0f6c87;
     --title-color: #322153;
     --text-color: #101114;
+    --light-color: #eee;
+    --paper-background-color: #fff;
+
+    @media (min-width: 700px) {
+      font-size: 62.5%;
+    }
   }
 
   * {
@@ -18,18 +26,15 @@ export default createGlobalStyle`
   body {
     background: #F0F0F5;
     color: var(--text-color);
-    color: #101114;
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, button {
-    font-family: Roboto, Arial, Helvetica, sans-serif;
-    font-size: 16px;
+    font: 500 1.6rem Roboto, Arial, Helvetica, sans-serif;
   }
 
   h1, h2, h3, h4, h5, h6 {
     color: var(--title-color);
-    font-family: Roboto;
   }
 
   button {
